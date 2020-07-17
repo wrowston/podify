09024d7e21811b7e31cfa0f15c51c06167ecd3a5
Will Rowston <willrowston@gmail.com>
2020-07-17 16:53:23 -0400

    added models

--- podify_app/models.py
+++ podify_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Creator(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Episode(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date_uploaded = models.DateTimeField('date published')
+    audio_url = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+
+class Podcast(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.ForeignKey(
+        Creator, on_delete=models.CASCADE, related_name='creator')
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    image_url = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
